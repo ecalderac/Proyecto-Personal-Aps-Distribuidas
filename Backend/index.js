a9e8ddaf772b07993c3799cee4d30e4f20cd092b
Eduardo Caldera Coltters <e.calderac@hotmail.com>
2020-08-18 16:47:26 -0400

    conexion a mongodb atlas con docker

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,23 @@
 'use strict'
 
-var mongoose = require('mongoose');   //cargando modulo de MONGODB
-var app = require('./app');
-var port = process.env.PORT || 3977; //puerto del servidor web
+const mongoose = require('mongoose'); //cargando modulo de MONGODB
+const app = require('./app');
+const port = process.env.PORT || 3000; //puerto del servidor web
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/BD_NFC_FLUTTER', (err, res) => {
-    if(err)
-    {
+mongoose.connect('mongodb+srv://adminecalderac:Q5NyP0QKhBk9Fxcf@cluster0.my1hc.mongodb.net/BD_NFC_FLUTTER', { useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
+    if (err) {
         throw err;
-    }
-    else
-    {
-        console.log("Base de Datos corriendo correctamente");
-        app.listen(port, function(){
-            console.log("Servidor del API REST de NFC+FLUTTER escuchando en hhtp://localhost:"+port);
+    } else {
+        console.log("Conexion establecida con la Base de Datos: MongoDB-Atlas");
+        app.listen(port, function() {
+            console.log("Servidor del API REST de NFC+FLUTTER escuchando en puerto: " + port);
         });
     }
 }); //conectando a la base de datos de mongodb
+
+//BD NUBE
+//mongodb+srv://adminecalderac:Q5NyP0QKhBk9Fxcf@cluster0.my1hc.mongodb.net/BD_NFC_FLUTTER
+
+//BD LOCAL
+//mongodb://localhost:27017/BD_NFC_FLUTTER
